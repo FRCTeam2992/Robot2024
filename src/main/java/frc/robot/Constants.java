5284f9c57748d5f52d1e82e645f13f196467def9
Gerad <geradg276@outlook.com>
2024-01-09 20:19:00 -0600

    Began creating basic shooter subsystem

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,16 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+/** Add your docs here. */
+public class Constants {
+
+    public static class Shooter{
+        public static class DeviceIDs{
+            public static int rightShooterMotorID = 0;
+            public static int leftShooterMotorID = 0;
+        }
+    }
+}
