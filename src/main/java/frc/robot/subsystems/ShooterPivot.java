e8019dde442f4519b507ef2cd11ebcff8acdde44
Gerad <geradg276@outlook.com>
2024-01-30 18:42:42 -0600

    started

---
+++ src/main/java/frc/robot/subsystems/ShooterPivot.java
@@ -0,0 +1,28 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ShooterPivot extends SubsystemBase {
+
+  private TalonFX pivotMotor;
+  private TalonFXConfiguration pivotMotorConfigs;
+
+  /** Creates a new ShooterPivot. */
+  public ShooterPivot() {
+
+    pivotMotor = new TalonFX(0, getName());
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
