4589efccb27845760cf1e34908e8b8e9c917b923
msonnier <mike@sonnier.name>
2024-03-01 13:58:06 -0600

    [Shooter] Add option to reset or retain motor PID controls

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -8,7 +8,7 @@ package frc.robot;
 
 public class Constants {
 
-    public static class Intake{
+    public static class Intake {
         public static final int intakeLeadMotorID = 10;
         public static final int intakeFollowMotorID = 11;
 
@@ -30,19 +30,21 @@ public class Constants {
         }
     }
 
-    public static class Shooter{
-        
-        public static class DeviceIDs{
-            public static int shooterMotorID = 8;
-         }
+    public static class Shooter {
 
-        public static class ShooterPIDConstants{
-            public static double P = 0.0001; //Place holder
-            public static double I = 0.0; //Place holder
-            public static double D = 0.0; //Place holder
-            public static double V = 0.0; //Place holder
-            public static double acceleration = 0.0; //Place holder
-            public static double cruiseVelocity = 0.0; //Place holder
+        public static class DeviceIDs {
+            public static int shooterMotorID = 8;
+        }
+
+        public static class ShooterPIDConstants {
+            public static final boolean useCodePID = false; // Whether to overwrite PID values in motor
+
+            public static double P = 0.0001; // Place holder
+            public static double I = 0.0; // Place holder
+            public static double D = 0.0; // Place holder
+            public static double V = 0.0; // Place holder
+            public static double acceleration = 0.0; // Place holder
+            public static double cruiseVelocity = 0.0; // Place holder
 
         }
     }
@@ -50,23 +52,24 @@ public class Constants {
     public static class ShooterPivot {
         public static final int pivotMotorID = 7;
 
-        public static final double pivotTargetedThreshold = 0.2; //Place holder
+        public static final double pivotTargetedThreshold = 0.2; // Place holder
 
-        public static final double pivotHeight = 14.9397244; //Pivot axis height above ground when elevator at 0 (in) 
-        public static final double shooterLength = 13.342337; //pivot axis to top flywheel axis
+        public static final double pivotHeight = 14.9397244; // Pivot axis height above ground when elevator at 0 (in)
+        public static final double shooterLength = 13.342337; // pivot axis to top flywheel axis
         public static final double flyWheelRadius = 2.0;
         public static final double robotMaxHeight = 47; // 1 inch wiggle room
 
-        public static final double pivotLevelOffset = 0.0; //place holder // degrees added to pivot position to find position where level = 0 
+        public static final double pivotLevelOffset = 0.0; // place holder // degrees added to pivot position to find
+                                                           // position where level = 0
 
         public static final double pivotGearRatio = 60.0;
 
         public static class PIDController {
             public static final boolean useCodePID = false; // Whether to overwrite PID values in motor
-            public static final double P = 0.00001; //Place holder
-            public static final double I = 0.0; //Place holder
-            public static final double D = 0.000; //Place holder
-            public static final double F = 0.01; //Place holder
+            public static final double P = 0.00001; // Place holder
+            public static final double I = 0.0; // Place holder
+            public static final double D = 0.000; // Place holder
+            public static final double F = 0.01; // Place holder
 
             public static final double maxVelocity = 30.0; // degrees/second //Place holder
             public static final double maxAcceleration = 60.0; // degrees/second^2 //Place holder
@@ -74,80 +77,83 @@ public class Constants {
             public static final double peakForwardDutyCycle = 0.4; // Max forward motor power
             public static final double peakReverseDutyCycle = -0.2; // Max reverse power
 
-            public static final double pivotAngleTolerance = 0.0; //Place holder
+            public static final double pivotAngleTolerance = 0.0; // Place holder
         }
 
         public static class Limits {
-            public static final double minPivotAngle = 1.5; //PlaceHolder
-            public static final double maxPivotAngle = 60.0; //PlaceHolder
+            public static final double minPivotAngle = 1.5; // PlaceHolder
+            public static final double maxPivotAngle = 60.0; // PlaceHolder
 
             public static final double maxPivotAmp = 10.0; // Max pivot angle when in Amp mode
-            public static final double maxPivotEndgame = 1.5;  // Max pivot angle in in Endgame mode
+            public static final double maxPivotEndgame = 1.5; // Max pivot angle in in Endgame mode
 
-            public static final double pivotCollisionZone = 10.0; //PlaceHolder
-            }
+            public static final double pivotCollisionZone = 10.0; // PlaceHolder
+        }
 
         public static class Positions {
-            public static final double intakingPiece = 15.0; //Place holder
-            public static final double pivotSafeZone = 13.0; //PlaceHolder
+            public static final double intakingPiece = 15.0; // Place holder
+            public static final double pivotSafeZone = 13.0; // PlaceHolder
         }
     }
 
     public static class Elevator {
-        public static final int leadMotorID = 2; //left
-        public static final int followMotor1ID = 3; //left
-        public static final int followMotor2ID = 4; //right
-        public static final int followMotor3ID = 5; //right
+        public static final int leadMotorID = 2; // left
+        public static final int followMotor1ID = 3; // left
+        public static final int followMotor2ID = 4; // right
+        public static final int followMotor3ID = 5; // right
 
-        public static final int leadMotorPDHPort = 0; //place holder
+        public static final int leadMotorPDHPort = 0; // place holder
 
-        public static final double gearRatio = (12.0/60.0) * (36.0/64.0); 
-        public static final double sprocketPitchDiameter = 1.751; //inches
-        public static final double encoderToInches = gearRatio * (sprocketPitchDiameter * Math.PI) * 2; //calculates instage which is double the motion of outer stage
+        public static final double gearRatio = (12.0 / 60.0) * (36.0 / 64.0);
+        public static final double sprocketPitchDiameter = 1.751; // inches
+        public static final double encoderToInches = gearRatio * (sprocketPitchDiameter * Math.PI) * 2; // calculates
+                                                                                                        // instage which
+                                                                                                        // is double the
+                                                                                                        // motion of
+                                                                                                        // outer stage
 
-        public static class PIDConstants{
-            public static final double kP = 2.5; //placeholder
-            public static final double kI = 0.0; //placeholder
-            public static final double kD = 0.0; //placeholder
-            public static final double kIZone = 0.0; //placeholder
-            public static final double kF = 0.025; //Place holder
+        public static class PIDConstants {
+            public static final double kP = 2.5; // placeholder
+            public static final double kI = 0.0; // placeholder
+            public static final double kD = 0.0; // placeholder
+            public static final double kIZone = 0.0; // placeholder
+            public static final double kF = 0.025; // Place holder
 
-            public static final double kMinOutput = -0.2; //placeholder
-            public static final double kMaxOutput = 0.2; //placeholder
+            public static final double kMinOutput = -0.2; // placeholder
+            public static final double kMaxOutput = 0.2; // placeholder
 
-            public static final double SmartMotionMaxVel = 350.0; //placeholder
-            public static final double SmartMotionMinVel = -350.0; //placeholder
-            public static final double SmartMotionMaxAcc = 500.0; //placeholder
-            public static final double SmartMotionAllowedError = 0.1; //placeholder
+            public static final double SmartMotionMaxVel = 350.0; // placeholder
+            public static final double SmartMotionMinVel = -350.0; // placeholder
+            public static final double SmartMotionMaxAcc = 500.0; // placeholder
+            public static final double SmartMotionAllowedError = 0.1; // placeholder
 
+            public static final double positionTolerance = 0.0; // placeholder
 
-            public static final double positionTolerance = 0.0; //placeholder
-
-            public static final double integratorRangeMin = -.2; //placeholder (Copied from Rodrigue's Code)
-            public static final double integratorRangeMax = .2; //Place holder
+            public static final double integratorRangeMin = -.2; // placeholder (Copied from Rodrigue's Code)
+            public static final double integratorRangeMax = .2; // Place holder
         }
 
-        public static final double elevatorHeightToleranceInch = 0.5; //Place holder
+        public static final double elevatorHeightToleranceInch = 0.5; // Place holder
 
         public static class Limits {
-            public static final double softStopBottom = 1.0; //bottom soft stop (in) //Place holder
-            public static final double softStopTop = 24.5; //bottom soft stop (in) //PlaceHolder
-            public static final double hardStopTop = 25.0; //bottom soft stop (in) //Place holder
+            public static final double softStopBottom = 1.0; // bottom soft stop (in) //Place holder
+            public static final double softStopTop = 24.5; // bottom soft stop (in) //PlaceHolder
+            public static final double hardStopTop = 25.0; // bottom soft stop (in) //Place holder
 
             public static final double hardStopCurrentLimit = 10.0; // Place Holder
 
-            public static final double elevatorDangerZone = 7.0; //placeholder
-            public static final double intakeDangerZone = 2.0; //placeholder
+            public static final double elevatorDangerZone = 7.0; // placeholder
+            public static final double intakeDangerZone = 2.0; // placeholder
 
         }
 
         public static class Positions {
-            public static final double intakingPiece = 0.0; //Place holder
-            public static final double safeZoneTop = 7.5; //placeholder
+            public static final double intakingPiece = 0.0; // Place holder
+            public static final double safeZoneTop = 7.5; // placeholder
         }
 
         public static class Climb {
-            public static final double joyStickDeadBand = 0.2; 
+            public static final double joyStickDeadBand = 0.2;
         }
     }
 }
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -30,30 +30,52 @@ public class Shooter extends SubsystemBase {
 
   private StatusSignal<Double> shooterVelocity;
 
-
   /** Creates a new Shooter. */
   public Shooter() {
     shooterMotor = new TalonFX(Constants.Shooter.DeviceIDs.shooterMotorID);
     shooterMotorConfigs = new TalonFXConfiguration();
-    
+
     shooterMotorConfigs.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
     shooterMotorConfigs.MotorOutput.NeutralMode = NeutralModeValue.Coast;
-    shooterMotorConfigs.Slot0.kP = Constants.Shooter.ShooterPIDConstants.P;
-    shooterMotorConfigs.Slot0.kI = Constants.Shooter.ShooterPIDConstants.I;
-    shooterMotorConfigs.Slot0.kD = Constants.Shooter.ShooterPIDConstants.D;
-    shooterMotorConfigs.Slot0.kV = Constants.Shooter.ShooterPIDConstants.V;
-    shooterMotorConfigs.MotionMagic.MotionMagicAcceleration = Constants.Shooter.ShooterPIDConstants.acceleration;
-    shooterMotorConfigs.MotionMagic.MotionMagicCruiseVelocity = Constants.Shooter.ShooterPIDConstants.cruiseVelocity;
+
+    if (!Constants.Shooter.ShooterPIDConstants.useCodePID) {
+      // Make sure we copy old PID values in motor over so we don't lose them
+      TalonFXConfiguration oldConfig = new TalonFXConfiguration();
+      shooterMotor.getConfigurator().refresh(oldConfig); // Get the old config
+
+      // Copy all slot 0 & MM paramters
+      shooterMotorConfigs.Slot0.kA = oldConfig.Slot0.kA;
+      shooterMotorConfigs.Slot0.kD = oldConfig.Slot0.kD;
+      shooterMotorConfigs.Slot0.kG = oldConfig.Slot0.kG;
+      shooterMotorConfigs.Slot0.kI = oldConfig.Slot0.kI;
+      shooterMotorConfigs.Slot0.kP = oldConfig.Slot0.kP;
+      shooterMotorConfigs.Slot0.kS = oldConfig.Slot0.kS;
+      shooterMotorConfigs.Slot0.kV = oldConfig.Slot0.kV;
+      shooterMotorConfigs.Slot0.StaticFeedforwardSign = oldConfig.Slot0.StaticFeedforwardSign;
+      shooterMotorConfigs.Slot0.GravityType = oldConfig.Slot0.GravityType;
+      shooterMotorConfigs.MotionMagic.MotionMagicAcceleration = oldConfig.MotionMagic.MotionMagicAcceleration;
+      shooterMotorConfigs.MotionMagic.MotionMagicCruiseVelocity = oldConfig.MotionMagic.MotionMagicCruiseVelocity;
+      shooterMotorConfigs.MotorOutput.PeakForwardDutyCycle = oldConfig.MotorOutput.PeakForwardDutyCycle;
+      shooterMotorConfigs.MotorOutput.PeakReverseDutyCycle = oldConfig.MotorOutput.PeakReverseDutyCycle;
+    } else {
+      shooterMotorConfigs.Slot0.kP = Constants.Shooter.ShooterPIDConstants.P;
+      shooterMotorConfigs.Slot0.kI = Constants.Shooter.ShooterPIDConstants.I;
+      shooterMotorConfigs.Slot0.kD = Constants.Shooter.ShooterPIDConstants.D;
+      shooterMotorConfigs.Slot0.kV = Constants.Shooter.ShooterPIDConstants.V;
+      shooterMotorConfigs.MotionMagic.MotionMagicAcceleration = Constants.Shooter.ShooterPIDConstants.acceleration;
+      shooterMotorConfigs.MotionMagic.MotionMagicCruiseVelocity = Constants.Shooter.ShooterPIDConstants.cruiseVelocity;
+    }
+
     shooterMotor.getConfigurator().apply(shooterMotorConfigs);
 
-    percentOutControlRequest = new DutyCycleOut(0.0);
-    velocityControlRequest = new VelocityDutyCycle(0.0);
-    motionMagicVelocityControlRequest = new MotionMagicVelocityDutyCycle(0.0);
+    percentOutControlRequest = new DutyCycleOut(0.0).withEnableFOC(true);
+    velocityControlRequest = new VelocityDutyCycle(0.0).withEnableFOC(true).withSlot(0);
+    motionMagicVelocityControlRequest = new MotionMagicVelocityDutyCycle(0.0).withEnableFOC(true).withSlot(0);
 
     shooterVelocity = shooterMotor.getVelocity();
 
   }
-  
+
   @Override
   public void periodic() {
     SmartDashboard.putData(this);
@@ -63,7 +85,7 @@ public class Shooter extends SubsystemBase {
     // This method will be called once per scheduler run
   }
 
-  public void setShooterSpeed (double percentSpeed) {
+  public void setShooterSpeed(double percentSpeed) {
     percentOutControlRequest.Output = percentSpeed;
     shooterMotor.setControl(percentOutControlRequest);
   }
@@ -72,25 +94,26 @@ public class Shooter extends SubsystemBase {
     this.shooterTargetRPM = shooterSetRPM / 60.0;
   }
 
-  public void setShooterToTargetRPM () {
+  public void setShooterToTargetRPM() {
     double speed = shooterTargetRPM;
     setShooterRawVelocity(speed);
   }
 
-  public void setShooterRawVelocity (double velocity) {
+  public void setShooterRawVelocity(double velocity) {
     velocityControlRequest.Velocity = velocity;
     shooterMotor.setControl(velocityControlRequest);
   }
 
-  public double getShooterRPM (){
-    return shooterVelocity.waitForUpdate(0.0).getValue() * 60.0;
+  public double getShooterRPM() {
+    // return shooterVelocity.waitForUpdate(0.0).getValue() * 60.0;
+    return shooterVelocity.refresh().getValue() * 60.0;
   }
 
-  public double getShooterTargetRPM () {
+  public double getShooterTargetRPM() {
     return shooterTargetRPM;
   }
 
-  public boolean atShooterRPM () {
+  public boolean atShooterRPM() {
     return (Math.abs(getShooterTargetRPM() - getShooterRPM()) < 200.0);
   }
 }
