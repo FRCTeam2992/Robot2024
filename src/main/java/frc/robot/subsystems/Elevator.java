7d12b7825694ef3ee77a1cb7332a5604f6277731
Gerad <72838769+GeradAlt-F4@users.noreply.github.com>
2024-03-05 21:08:41 -0600

    Update src/main/java/frc/robot/subsystems/Elevator.java
    Co-authored-by: Ryan B. Harvey <ryan.b.harvey@gmail.com>

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -7,9 +7,6 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkBase.ControlType;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
-
-import javax.swing.text.StyleContext.SmallAttributeSet;
-
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkPIDController;
 
