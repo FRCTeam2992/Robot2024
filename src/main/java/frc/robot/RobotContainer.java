98224fbb1207f4893b2d8b308be3ae4495dbadb9
msonnier <mike@sonnier.name>
2024-03-06 01:19:42 -0600

    2 note center auto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -294,5 +294,7 @@ public class RobotContainer {
     NamedCommands.registerCommand("stopIntake", new StopIntake(mIntake));
     NamedCommands.registerCommand("stopFeeder", new MoveFeeder(mFeeder, 0, false));
     NamedCommands.registerCommand("stopPivot", new StopShooterPivot(mShooterPivot));
+    NamedCommands.registerCommand("moveFwd1.5s", new AutoMoveForwardBack(mDrivetrain, true, 1).withTimeout(2.5));
+    NamedCommands.registerCommand("driveStop", new AutoMoveForwardBack(mDrivetrain, true, 0).withTimeout(0.5));
   }
 }
--- src/main/java/frc/robot/commands/AutoMoveForwardBack.java
+++ src/main/java/frc/robot/commands/AutoMoveForwardBack.java
@@ -35,7 +35,9 @@ public class AutoMoveForwardBack extends Command {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    mDrivetrain.stopDrive();
+  }
 
   // Returns true when the command should end.
   @Override
