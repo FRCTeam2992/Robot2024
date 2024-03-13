5a6bf71b084c468857789f813aef14ff268a31fc
Gerad <geradg276@outlook.com>
2024-03-13 18:20:45 -0500

    Autos

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -283,7 +283,7 @@ public class RobotContainer {
     mNoteInterpolator.addDataPoint(new NoteDataPoint(66, 3100, 41, 0.0));
     mNoteInterpolator.addDataPoint(new NoteDataPoint(78, 3100, 38, 0.0));
     mNoteInterpolator.addDataPoint(new NoteDataPoint(89, 3300, 35.0, 0.0));
-    mNoteInterpolator.addDataPoint(new NoteDataPoint(99, 3700, 33.0, 0.0));
+    mNoteInterpolator.addDataPoint(new NoteDataPoint(99, 3700, 31.75, 0.0));
     mNoteInterpolator.addDataPoint(new NoteDataPoint(112, 4000, 31.0, 0.0));
     mNoteInterpolator.addDataPoint(new NoteDataPoint(120, 4000, 29.0, 0.0));
     mNoteInterpolator.addDataPoint(new NoteDataPoint(134, 4000, 27.0, 0.0));
